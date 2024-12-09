cd3595c0b02eb8cecc16f2af7c6bbf4dd9ff5458
Miki56788 <amiki.ahmetova@bk.ru>
2024-12-09 17:43:10 +0500

    simple message in another file

---
+++ src/Users/SimpleMessage.java
@@ -0,0 +1,21 @@
+package Users;
+
+public class SimpleMessage extends Message {
+
+    public SimpleMessage() {
+        super();
+    }
+
+    public SimpleMessage(Employee sender, Employee receiver, String content) {
+        super(sender, receiver, content);
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleMessage{" +
+                "sender=" + getSender() +
+                ", receiver=" + getReceiver() +
+                ", content='" + getContent() + '\'' +
+                '}';
+    }
+}
