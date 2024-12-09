a44683a97491c33f46a874b0d93177aa76f1beb6
Miki56788 <amiki.ahmetova@bk.ru>
2024-12-09 22:19:08 +0500

    JUst messsage factory

---
+++ src/Users/MessageFactory.java
@@ -0,0 +1,19 @@
+package Users;
+
+public class MessageFactory {
+
+    public Message getMessage(String type) {
+        switch (type) {
+            case "Complaint":
+                return new Complaint();
+            case "WorkMessage":
+                return new WorkMessage();
+            case "Request":
+                return new Request();
+            case "SimpleMessage":
+                return new SimpleMessage();
+            default:
+                throw new IllegalArgumentException("Неизвестный тип сообщения: " + type);
+        }
+    }
+}
