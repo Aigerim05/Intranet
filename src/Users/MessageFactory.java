fbde14809b1cd543079d685ec5e926a3ee0f6838
Miki56788 <amiki.ahmetova@bk.ru>
2024-12-13 23:09:26 +0500

    Добавлена проверка типа caller, чтобы убедиться, что только Teacher может создавать объект Complaint.

--- src/Users/MessageFactory.java
+++ src/Users/MessageFactory.java
@@ -1,11 +1,19 @@
-package Users;
+package Attributes;
+
+import Interfaces.Employee;
+import Users.Teacher;
 
 public class MessageFactory {
 
-    public Message getMessage(String type) {
+    public Message getMessage(String type, Employee caller) {
         switch (type) {
             case "Complaint":
-                return new Complaint();
+                if (caller instanceof Teacher) {
+                    return new Complaint();
+                } else {
+                    // Возвращаем простое сообщение или выводим сообщение об ошибке
+                    return new SimpleMessage(null, null, "Access Denied");
+                }
             case "WorkMessage":
                 return new WorkMessage();
             case "Request":
