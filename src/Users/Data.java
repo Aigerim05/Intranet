f297e74259fb94c5c0230e69663950eab24917c0
Aigerim05 <aigerim.darmesh@gmail.com>
2024-12-15 16:56:22 +0500

    added Data class

--- src/Users/Data.java
+++ src/Users/Data.java
@@ -1,3 +1,5 @@
+package Users;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -6,51 +8,56 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Vector;
 
-
-
+import Attributes.News;
 
 public class Data implements Serializable {
-	ArrayList<User> users = new ArrayList<User>();
-	ArrayList<Course> courses = new ArrayList<Course>();
-	ArrayList<Student> students = new ArrayList<Student>();
-	ArrayList<Teacher> teachers = new ArrayList<Teacher>();
-	ArrayList<Manager> managers = new ArrayList<Manager>();
-	ArrayList<News> news = new ArrayList<News>();
+
+	private static final long serialVersionUID = 1L;
+
+	private static Data INSTANCE;
+
+	public ArrayList<User> users = new ArrayList<>();
+	public ArrayList<Course> courses = new ArrayList<>();
+	public ArrayList<News> news = new ArrayList<>();
+	public ArrayList<Teacher> teachers = new ArrayList<>();
+	public ArrayList<Student> students = new ArrayList<>();
+	public ArrayList<Manager> managers = new ArrayList<>();
+	public ArrayList<Admin> admins = new ArrayList<>();
+	//public ArrayList<Researcher> researchers = new ArrayList<>();
+	//public ArrayList<ResearchJournal> researchJournals = new ArrayList<>();
+
+	static{
+		if(new File("data.ser").exists()){
+			try {
+				INSTANCE = read();
+			} catch (IOException | ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		} else {
+			INSTANCE = new Data();
+		}
+	}
+	private Data() {}
 
 
+	public static synchronized Data getInstance() {
+		return INSTANCE;
+	}
 
-	public static Data INSTANCE;
-	//	static {
-	//		if(new File("data").exists()) {
-	//			try {
-	//				INSTANCE = read();
-	//			} catch (Exception e) {
-	//				e.printStackTrace();
-	//			}
-	//		} else {
-	//			INSTANCE = new Data();
-	//		}
-	//	}
-	//	private Data() {
-	//
-	//	}
-	//	public static Data read() throws IOException, ClassNotFoundException{
-	//		FileInputStream fis = new FileInputStream("data");
-	//		ObjectInputStream oin = new ObjectInputStream(fis);
-	//		return (Data) oin.readObject();
-	//	}
-	//	public static void write()throws IOException{
-	//		FileOutputStream fos = new FileOutputStream("data");
-	//		ObjectOutputStream oos = new ObjectOutputStream(fos);
-	//		oos.writeObject(INSTANCE);
-	//		oos.close();
-	//	}
-	//	public static int nextId() {
-	//		// TODO Auto-generated method stub
-	//		return  INSTANCE.students.size()+1;
-}
+	public static Data read() throws IOException, ClassNotFoundException {
+		try (FileInputStream fis = new FileInputStream("data.ser");
+				ObjectInputStream oin = new ObjectInputStream(fis)) {
+			return (Data) oin.readObject();
+		}
+	}
 
-
+	public static synchronized void write() throws IOException {
+		if (INSTANCE != null) {
+			try (FileOutputStream fos = new FileOutputStream("data.ser");
+					ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+				oos.writeObject(INSTANCE);
+			}
+		}
+	}
 }
