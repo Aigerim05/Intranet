e5546d7ecfa4283657092994d850630c012ff30c
Aigerim05 <aigerim.darmesh@gmail.com>
2024-12-16 19:19:21 +0500

    deleted data.ser

--- src/Users/UserOperation.java
+++ src/Users/UserOperation.java
@@ -8,28 +8,62 @@ public class UserOperation {
 	public UserOperation() {};
 
 	public static Department enterDepartment(int choice) {
-		if(choice == 1) {
+		switch (choice) {
+		case 1:
 			return Department.SITE;
+		case 2:
+			return Department.ISE;
+		case 3:
+			return Department.SEOGI;
+		case 4:
+			return Department.SG;
+		case 5:
+			return Department.BS;
+		case 6:
+			return Department.KMA;
+		case 7:
+			return Department.SAM;
+		case 8:
+			return Department.SCE;
+		case 9:
+			return Department.SMSGT;
+		default:
+			return null;  
 		}
-		// нужно дописать сюда все департаменты
-		return null; // null нужно убрать чтобы не было nullPointerException
 	}
 
+
 	public static Language enterLanguage(int choice) {
-		if(choice == 1) {
+		switch (choice) {
+		case 1:
 			return Language.KZ;
+		case 2:
+			return Language.RU;
+		case 3:
+			return Language.EN;
+		default:
+			return null;  
 		}
-		// нужно дописать сюда все languages
-		return null; // null нужно убрать чтобы не было nullPointerException
 	}
+
 
 
 	public static Position enterPosition(int choice) {
-		if(choice == 1) {
+		switch (choice) {
+		case 1:
 			return Position.PROFESSOR;
+		case 2:
+			return Position.SENIOR_LECTURER;
+		case 3:
+			return Position.ASSISTANT_PROFESSOR;
+		case 4:
+			return Position.TUTOR;
+		case 5:
+			return Position.ASSOCIATE_PROFESSOR;
+		default:
+			return null;  
 		}
-		// нужно дописать сюда все languages
-		return null; // null нужно убрать чтобы не было nullPointerException
 	}
+
 
 }
